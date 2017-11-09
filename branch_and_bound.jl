a0ae32ae89dd2511c5ceb613541b16b72b2cb63a
cgamboa <caagamboaro2unal.edu.co>
2017-11-09 17:20:14 -0200

    Adicionei uma função pra selecionar um problema da lista. Converti em função cada paso do algoritmo

--- branch_and_bound.jl
+++ branch_and_bound.jl
@@ -10,7 +10,6 @@ mutable struct nodo
   frac::Vector{Float64}
   x_int::Vector{Float64}
   status::Symbol
-  visit::Int64
 end
 
 function fractional(x)
@@ -32,26 +31,25 @@ function branch(x,var_bin)
   return i
 end
 
-function poda(lista_,sense,liminf,limsup,V)
+function poda(lista_,sense,liminf,limsup, P)
   L = size(lista_)[1]
-  Visit = []
-  V_ = 0
   erase = []
+  P_ = 0
   for l=1:L
     if lista_[l].status == :Infeasible
       erase = copy([erase;[l]])
+      P_ = P_ + 1
     elseif lista_[l].frac == 0
       erase = copy([erase;[l]])
+      P_ = P_ + 1
     elseif (sense == :Max && lista_[l].sup <= liminf) ||(sense == :Min && lista_[l].inf >= limsup)
        erase = copy([erase;[l]])
-     elseif lista_[l].visit == 1
-      erase = copy([erase;[l]])
-      V_ = copy(V_) + 1
+       P_ = P_ + 1
     end
   end
-  Visit = V_ + V
+  P =copy(P + P_)
   lista = copy(deleteat!(lista_,erase))
-  return lista, Visit
+  return lista, P
 end
 
 function deep(m::JuMP.Model,var_bin,frac)
@@ -106,6 +104,124 @@ function deep(m::JuMP.Model,var_bin,frac)
 return liminf, limsup, x_best,N
 end
 
+function filhos(pai,lista_,var_bin,sense,liminf,limsup)
+  lista = copy(lista_)
+  #############################################################################
+  # Nodo filho 1
+  #############################################################################
+    m1 = copy(pai.Model)
+    frac = copy(pai.frac)
+    i_ = branch(frac,var_bin)
+    m1.colUpper[i_] = 0
+    status = solve(m1, relaxation=true)
+    x = copy(m1.colVal)
+    obj = copy(m1.objVal)
+    frac = fractional(x)
+
+    if sense == :Max
+      sup = copy(obj)
+      inf = liminf
+    else
+      inf = copy(obj)
+      sup = limsup
+    end
+
+    if frac == zeros(length(x))
+      x_int = copy(x)
+    else
+      x_int = []
+    end
+  nivel = pai.nivel + 1
+  filho1 = nodo(nivel,m1,inf,sup,x,frac,x_int,status)
+  push!(lista,filho1)
+
+  #############################################################################
+  # Nodo filho 2
+  #############################################################################
+  m2 = copy(pai.Model)
+  m2.colLower[i_] = 1
+  status = solve(m2, relaxation=true)
+  x = copy(m2.colVal)
+  obj = copy(m2.objVal)
+  frac = fractional(x)
+
+  if sense == :Max
+    sup = copy(obj)
+    inf = copy(liminf)
+  else
+    inf = copy(obj)
+    sup = copy(limsup)
+  end
+
+  if frac == zeros(length(x))
+    x_int = copy(x)
+  else
+    x_int = []
+  end
+
+  filho2 = nodo(nivel,m2,inf,sup,x,frac,x_int,status)
+  push!(lista,filho2)
+return lista
+end
+
+function selection(lista,sense,liminf,limsup)
+  pai = []
+  l = []
+  L = length(lista)
+  for i=1:L
+    if sense == :Max && lista[i].sup == limsup
+       pai = lista[i]
+       l = i
+    elseif sense == :Min && lista[i].inf == liminf
+      pai = lista[i]
+      l = i
+    end
+  end
+  return pai, l
+end
+
+function gap(lista,sense,liminf,limsup,AUX,int_sol)
+  ##############################################################################
+  # Gap
+  ##############################################################################
+
+  SUP = []
+  INFI = []
+
+
+  L = length(lista)
+    if sense ==:Max
+      for l=1:L
+        if  lista[l].status != :Infeasible && lista[l].status != :Unbounded
+          SUP = copy([SUP;[lista[l].sup]])
+        end
+        if lista[l].x_int != []
+            int_sol = copy([int_sol;[lista[l].sup]])
+            AUX = copy([AUX;Matrix([lista[l].sup lista[l].x_relax'])])
+          else
+            int_sol = copy([int_sol;[liminf]])
+        end
+      end
+      limsup = maximum(SUP)
+      liminf = maximum(int_sol)
+    else
+      for l=1:L
+        if lista[l].status != :Infeasible && lista[l].status != :Unbounded
+         INFI = copy([INFI;[lista[l].inf]])
+       end
+        if lista[l].x_int != []
+          int_sol = copy([int_sol;[lista[l].inf]])
+          AUX = copy([AUX;Matrix([lista[l].inf lista[l].x_relax'])])
+        else
+          int_sol = copy([int_sol;[limsup]])
+        end
+      end
+      liminf = minimum(INFI)
+      limsup = minimum(int_sol)
+    end
+    return liminf, limsup, AUX, int_sol
+end
+
 
 function BNB(m::JuMP.Model)
 
@@ -113,6 +229,10 @@ function BNB(m::JuMP.Model)
   # Nodo raiz
   #-----------------------------------------------------------------------------
   # inicialização da Lista
+  m.ext[:Tempo] = []
+  m.ext[:status] = []
+  m.ext[:Visit] = []
+  m.ext[:Podas] = []
   Tempo = []
   lista = []
   sol = []
@@ -139,15 +259,17 @@ function BNB(m::JuMP.Model)
 
   if frac == zeros(length(x))
     println("Solução inteira achada")
-    return m_
+    m = copy(m_)
+    return nothing
   end
 
   if status == :Infeasible
     println("Problema inviável")
-    return m_
+    m = copy(m_)
+    return nothing
   end
 
- liminf, limsup, x_best, Max_iter = deep(m_,var_bin,frac)
+ liminf, limsup, x_best, Max_nivel = deep(m_,var_bin,frac)
 
   if sense == :Max
     sup = copy(obj)
@@ -159,7 +281,7 @@ function BNB(m::JuMP.Model)
     liminf = copy(obj)
   end
 
-raiz = nodo(0,m_,inf,sup,x,frac,[],status,0)
+raiz = nodo(0,m_,inf,sup,x,frac,[],status)
 
 # Dados iniciais
 nivel = 1
@@ -170,130 +292,27 @@ push!(lista, raiz)
 L = 1
 int_sol = []
 
-ϵ = limsup - liminf
+ϵ = abs(limsup - liminf)
 iter = 1
 Visit = 0
+Podas = 0
 
-if Max_iter  >= 50
-  Max_iter = 10
-end
+Max_iter =1000
 
-while ϵ >= 1.0e-1 && iter <= Max_iter && lista != []
+while ϵ >= 1.0e-1 && iter <= Max_iter
 
+  pai, l = selection(lista,sense,liminf,limsup)
+  deleteat!(lista,l)
+  Visit = Visit + 1
 
-  for l=1:L
+  lista  = filhos(pai,lista,var_bin,sense,liminf,limsup)
 
-    #############################################################################
-    # Nodo filho 1
-    #############################################################################
+  liminf, limsup, AUX, int_sol = gap(lista,sense,liminf,limsup,AUX,int_sol)
 
-      m1 = copy(lista[l].Model)
-      frac = copy(lista[l].frac)
-      i_ = branch(frac,var_bin)
-      m1.colUpper[i_] = 0
-      status = solve(m1, relaxation=true)
-      x = copy(m1.colVal)
-      obj = copy(m1.objVal)
-      frac = fractional(x)
+  lista, Podas = poda(lista,sense,liminf,limsup,Podas)
 
-      if sense == :Max
-        sup = copy(obj)
-        if frac == zeros(length(x))
-         inf = copy(obj)
-        else
-         inf = copy(liminf)
-        end
-      else
-        inf = copy(obj)
-        if frac == zeros(length(x))
-        sup = copy(obj)
-        else
-        sup = copy(limsup)
-        end
-      end
-
-      if frac == zeros(length(x))
-        x_int = copy(x)
-      else
-        x_int = []
-      end
-
-    filho1 = nodo(nivel,m1,inf,sup,x,frac,x_int,status,0)
-    push!(lista,filho1)
-
-    #############################################################################
-    # Nodo filho 2
-    #############################################################################
-    m2 = copy(lista[l].Model)
-    m2.colLower[i_] = 1
-    status = solve(m2, relaxation=true)
-    x = copy(m2.colVal)
-    obj = copy(m2.objVal)
-    frac = fractional(x)
-
-    if sense == :Max
-      sup = copy(obj)
-      inf = copy(liminf)
-    else
-      inf = copy(obj)
-      sup = copy(limsup)
-    end
-
-    if frac == zeros(length(x))
-      x_int = copy(x)
-    else
-      x_int = []
-    end
-
-    filho2 = nodo(nivel,m2,inf,sup,x,frac,x_int,status,0)
-    push!(lista,filho2)
-    lista[l].visit = 1
-  end
-  ##############################################################################
-  # Gap
-  ##############################################################################
-
-  SUP = []
-  INFI = []
-
-  L = size(lista)[1]
-    if sense ==:Max
-      for l=1:L
-        if lista[l].nivel == nivel && lista[l].status != :Infeasible
-          SUP = copy([SUP;[lista[l].sup]])
-        end
-        if lista[l].x_int != []
-            int_sol = copy([int_sol;[lista[l].inf]])
-            AUX = copy([AUX;Matrix([lista[l].inf lista[l].x_relax'])])
-          else
-            int_sol = copy([int_sol;[liminf]])
-        end
-      end
-      limsup = minimum(SUP)
-      liminf = maximum(int_sol)
-    else
-      for l=1:L
-        if lista[l].nivel == nivel && lista[l].status != :Infeasible
-         INFI = copy([INFI;[lista[l].inf]])
-       end
-        if lista[l].nivel == nivel && lista[l].x_int != []
-          int_sol = copy([int_sol;[lista[l].sup]])
-          AUX = copy([AUX;Matrix([lista[l].sup lista[l].x_relax'])])
-        else
-          int_sol = copy([int_sol;[limsup]])
-        end
-      end
-      liminf = maximum(INFI)
-      limsup = minimum(int_sol)
-    end
-
-
-    lista,Visit = poda(lista,sense,liminf,limsup,Visit)
-
-    L = size(lista)[1]
+   L = size(lista)[1]
    ϵ = limsup - liminf
-
-     nivel = nivel + 1
 
     if AUX != []
       if sense == :Max
@@ -316,21 +335,7 @@ while ϵ >= 1.0e-1 && iter <= Max_iter && lista != []
     end
     iter = copy(iter) + 1
 end
-
-liminf_ = []
-limsup_ = []
-
-if ϵ >= 1.0e-1 &&  iter > Max_iter && lista != []
-  l = rand(collect(1:size(lista)[1]))
-  liminf_, limsup_, x_best_ = deep(lista[l].Model,var_bin,lista[l].frac)
-  if sense == :Max && liminf_ >= liminf
-    liminf = copy(liminf_)
-    x_best = copy(x_best_)
-  elseif sense == :Min && limsup_ <= limsup
-    limsup = copy(limsup_)
-    x_best = copy(x_best_)
-  end
-end
+Tempo = toc()
 
 if iter - 1 == Max_iter &&  ϵ > 1.0e-1 && x_best != []
   status = :Subotimal
@@ -341,13 +346,20 @@ if iter - 1 == Max_iter &&  ϵ > 1.0e-1 && x_best != []
   end
 elseif iter - 1 == Max_iter && x_best == []
   status = :Nosolutionfound
+elseif lista == [] && x_best == []
+  status = :Infeasible
 else
   status = :Optimal
 end
+
 m.objVal = sol
 m.colVal = x_best
-m.objBound = maximum([ϵ;0])
-Tempo = toc()
-return m,status,Tempo, Visit
+m.objBound = maximum([ϵ 0])
+m.ext[:Tempo] = Tempo
+m.ext[:status] = status
+m.ext[:Visit] = Visit
+m.ext[:Podas] = Podas
+
+return nothing
 
 end
