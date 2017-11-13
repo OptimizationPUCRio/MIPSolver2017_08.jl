95f21675e27a4b1a44ef3dd8631f6f12a5306ecb
carlos <caagamboaro@unal.edu.co>
2017-11-13 19:35:41 -0200

    Fiz alguns corretivos

--- src/branch_and_bound.jl
+++ src/branch_and_bound.jl
@@ -12,23 +12,19 @@ mutable struct nodo
   status::Symbol
 end
 
-function fractional(x)
-  s = length(x)
+function fractional(x,var_bin)
+  s = length(var_bin)
   frac = Array{Float64,1}(s)
 
-  for i=1:s[1]
-    frac[i] = modf(x[i])[1]
+  for i=1:s
+    frac[i] = modf(x[var_bin[i]])[1]
   end
   return frac
 end
 
 function branch(x,var_bin)
-  aux = Inf*ones(size(x))
-  for i in var_bin
-    aux[i] = x[i]
-  end
-  f,i = findmin(abs(aux-0.5))
-  return i
+  f,i = findmin(abs(x-0.5))
+  return var_bin[i]
 end
 
 function poda(lista_,sense,liminf,limsup, P)
@@ -52,7 +48,7 @@ function poda(lista_,sense,liminf,limsup, P)
   return lista, P
 end
 
-function deep(m::JuMP.Model,var_bin,frac)
+function deep(m::JuMP.Model,var_bin,frac, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
   m_ = copy(m)
     N = 1
     liminf = []
@@ -63,20 +59,20 @@ function deep(m::JuMP.Model,var_bin,frac)
     k = 1
     sense = m_.objSense
     status = :Optimal
-    while frac != zeros(length(frac)) && status == :Optimal
+    while frac != zeros(length(var_bin)) && status == :Optimal
       i_ = branch(frac,var_bin)
       if mod(k,2) == 0
          m_.colUpper[i_] = 0
          status = solve(m_, relaxation=true)
          x = copy(m_.colVal)
          obj = copy(m_.objVal)
-         frac = fractional(x)
+         frac = fractional(x,var_bin)
       else
         m_.colLower[i_] = 1
         status = solve(m_, relaxation=true)
         x = copy(m_.colVal)
         obj = copy(m_.objVal)
-        frac = fractional(x)
+        frac = fractional(x,var_bin)
       end
       k = k + 1
       N = N + 1
@@ -104,7 +100,7 @@ function deep(m::JuMP.Model,var_bin,frac)
 return liminf, limsup, x_best,N
 end
 
-function filhos(pai,lista_,var_bin,sense,liminf,limsup)
+function filhos(pai,lista_,var_bin,sense,liminf,limsup, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
   lista = copy(lista_)
   #############################################################################
   # Nodo filho 1
@@ -116,7 +112,7 @@ function filhos(pai,lista_,var_bin,sense,liminf,limsup)
     status = solve(m1, relaxation=true)
     x = copy(m1.colVal)
     obj = copy(m1.objVal)
-    frac = fractional(x)
+    frac = fractional(x,var_bin)
 
     if sense == :Max
       sup = copy(obj)
@@ -126,7 +122,7 @@ function filhos(pai,lista_,var_bin,sense,liminf,limsup)
       sup = limsup
     end
 
-    if frac == zeros(length(x))
+    if frac == zeros(length(var_bin))
       x_int = copy(x)
     else
       x_int = []
@@ -143,7 +139,7 @@ function filhos(pai,lista_,var_bin,sense,liminf,limsup)
   status = solve(m2, relaxation=true)
   x = copy(m2.colVal)
   obj = copy(m2.objVal)
-  frac = fractional(x)
+  frac = fractional(x,var_bin)
 
   if sense == :Max
     sup = copy(obj)
@@ -153,7 +149,7 @@ function filhos(pai,lista_,var_bin,sense,liminf,limsup)
     sup = copy(limsup)
   end
 
-  if frac == zeros(length(x))
+  if frac  == zeros(length(var_bin))
     x_int = copy(x)
   else
     x_int = []
@@ -180,7 +176,7 @@ function selection(lista,sense,liminf,limsup)
   return pai, l
 end
 
-function gap(lista,sense,liminf,limsup,AUX,int_sol)
+function gap(lista,sense,liminf,limsup,AUX,int_sol,IntSol)
   ##############################################################################
   # Gap
   ##############################################################################
@@ -197,6 +193,7 @@ function gap(lista,sense,liminf,limsup,AUX,int_sol)
         end
         if lista[l].x_int != []
             int_sol = copy([int_sol;[lista[l].sup]])
+            IntSol = IntSol + 1
             AUX = copy([AUX;Matrix([lista[l].sup lista[l].x_relax'])])
           else
             int_sol = copy([int_sol;[liminf]])
@@ -211,6 +208,7 @@ function gap(lista,sense,liminf,limsup,AUX,int_sol)
        end
         if lista[l].x_int != []
           int_sol = copy([int_sol;[lista[l].inf]])
+          IntSol = IntSol + 1
           AUX = copy([AUX;Matrix([lista[l].inf lista[l].x_relax'])])
         else
           int_sol = copy([int_sol;[limsup]])
@@ -219,11 +217,11 @@ function gap(lista,sense,liminf,limsup,AUX,int_sol)
       liminf = minimum(INFI)
       limsup = minimum(int_sol)
     end
-    return liminf, limsup, AUX, int_sol
+    return liminf, limsup, AUX, int_sol,IntSol
 end
 
 
-function BNB(m::JuMP.Model)
+function BNB(m::JuMP.Model, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
 
   #-----------------------------------------------------------------------------
   # Nodo raiz
@@ -233,10 +231,13 @@ function BNB(m::JuMP.Model)
   m.ext[:status] = []
   m.ext[:Visit] = []
   m.ext[:Podas] = []
+  m.ext[:Iter] = []
+  m.ext[:IntegerSolutions] = []
   Tempo = []
   lista = []
   sol = []
   x_best = []
+  IntSol = []
   AUX = []
   # Relaxação linear
   tic()
@@ -255,21 +256,41 @@ function BNB(m::JuMP.Model)
     end
   end
 
-  frac = fractional(x)
+  frac = fractional(x,var_bin)
 
-  if frac == zeros(length(x))
+  if frac == zeros(length(var_bin))
     println("Solução inteira achada")
-    m = copy(m_)
+    m.objVal = copy(obj)
+    m.colVal = copy(x)
+    m.objBound = 0
+    m.ext[:Tempo] = 0
+    m.ext[:status] = status
+    m.ext[:Visit] = 0
+    m.ext[:Podas] = 0
+    m.ext[:Iter] = 0
+    m.ext[:IntegerSolutions] = 1
     return nothing
   end
 
   if status == :Infeasible
     println("Problema inviável")
-    m = copy(m_)
+    m.objVal = :Infeasible
+    m.colVal = []
+    m.objBound = 0
+    m.ext[:Tempo] = 0
+    m.ext[:status] = status
+    m.ext[:Visit] = 0
+    m.ext[:Podas] = 0
+    m.ext[:Iter] = 0
+    m.ext[:IntegerSolutions] = 0
     return nothing
   end
 
  liminf, limsup, x_best, Max_nivel = deep(m_,var_bin,frac)
+
+ if x_best != []
+   IntSol = 1
+ end
 
   if sense == :Max
     sup = copy(obj)
@@ -292,7 +313,7 @@ push!(lista, raiz)
 L = 1
 int_sol = []
 
-ϵ = abs(limsup - liminf)
+ϵ = limsup - liminf
 iter = 1
 Visit = 0
 Podas = 0
@@ -313,7 +334,7 @@ while ϵ >= 1.0e-1 && iter <= Max_iter
   #############################################################################
   # Atualização do lowere (upper bound) para o problema de Min (Max)
   #############################################################################
-  liminf, limsup, AUX, int_sol = gap(lista,sense,liminf,limsup,AUX,int_sol)
+  liminf, limsup, AUX, int_sol, IntSol = gap(lista,sense,liminf,limsup,AUX,int_sol,IntSol)
   #############################################################################
   # Podas da árvore
   #############################################################################
@@ -371,6 +392,8 @@ m.ext[:Tempo] = Tempo
 m.ext[:status] = status
 m.ext[:Visit] = Visit
 m.ext[:Podas] = Podas
+m.ext[:Iter] = iter
+m.ext[:IntegerSolutions] = IntSol
 
 return nothing
 
