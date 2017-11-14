a365953360b3140de51b70ef1f64a12a4ef7ed70
carlos <caagamboaro@unal.edu.co>
2017-11-14 19:32:14 -0200

    arquivo de testes

---
+++ src/runtest.jl
@@ -0,0 +1,15 @@
+
+
+test1(BNB,GurobiSolver(OutputFlag=0))
+test2(BNB,GurobiSolver(OutputFlag=0))
+test3(BNB,GurobiSolver(OutputFlag=0))
+testCaminho(BNB,GurobiSolver(OutputFlag=0))
+testInfeasibleKnapsack(BNB,GurobiSolver(OutputFlag=0))
+testRobustCCUC(BNB,GurobiSolver(OutputFlag=0))
+test_P1_Brito(BNB,GurobiSolver(OutputFlag=0))
+test_PL_Infeasible_Brito(BNB,GurobiSolver(OutputFlag=0))
+test_PL_Simples_Brito(BNB,GurobiSolver(OutputFlag=0))
+test_MIP_Minimal_Brito(BNB,GurobiSolver(OutputFlag=0))
+test_MIP_Pequeno_Brito(BNB,GurobiSolver(OutputFlag=0))
+test_PL_Unbounded_Brito(BNB,GurobiSolver(OutputFlag=0))
+  
